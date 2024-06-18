2a40d238f58bd4ce518c5e4b23ec72eac044252c
مصطفي احمد عباس العشماوي <96683689+mostafa2855@users.noreply.github.com>
2024-06-18 15:48:26 +0300

    Add files via upload

--- caeaser task/caesar.py
+++ caeaser task/caesar.py
@@ -1,37 +1,33 @@
 def caesar_cipher(text, shift, mode):
     result = ""
 
-    
     if mode == 'decrypt':
         shift = -shift
 
-    
     for char in text:
         if char.isalpha():
-             
             start = ord('A') if char.isupper() else ord('a')
-             
+           
             result += chr((ord(char) - start + shift) % 26 + start)
         else:
-            
             result += char
 
     return result
 
 def main():
     while True:
-        
-        mode = input("Would you like to Encrypt or Decrypt or Exit the program? ").lower()
+         
+        mode = input("Would you like to (encrypt) or (decrypt) or (exit) the program? ").lower()
         if mode == 'exit':
             print("Exiting the program.")
             break
-        elif mode not in ['Encrypt', 'Decrypt']:
-            print("Invalid mode. Please choose 'Encrypt' or 'Decrypt' or 'Exit'.")
+        elif mode not in ['encrypt', 'decrypt']:
+            print("Invalid mode. Please choose 'encrypt' or 'decrypt' or 'exit'.")
             continue
 
-         
+        
         try:
-            shift = int(input("Enter the kay value: "))
+            shift = int(input("Enter the shift value: "))
         except ValueError:
             print("Invalid shift value. Please enter an integer.")
             continue
@@ -39,7 +35,7 @@ def main():
          
         text = input("Enter the text to be processed: ")
 
-         
+        
         result = caesar_cipher(text, shift, mode)
         print(f"The result is: {result}")
 
